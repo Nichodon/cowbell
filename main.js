6b17b8939251cc6ee403cdc9748baee671570321
Nichodon <brandonsychung@gmail.com>
2018-05-06 10:35:52 -0700

    add icon

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600, frame: false})
+    win = new BrowserWindow({width: 800, height: 600, frame: false, icon: 'cowbell.png'})
   
     // and load the index.html of the app.
     win.loadURL(url.format({
