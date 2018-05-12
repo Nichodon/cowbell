c98f7f653923b4ebb271ee24ab520fe17ef0bba3
Nichodon <brandonsychung@gmail.com>
2018-05-12 15:12:50 -0700

    edit window parameters

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const {app, BrowserWindow} = require('electron')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600, frame: false, icon: 'cowbell.png'})
+    win = new BrowserWindow({
+      width: 1024,
+      height: 768,
+      frame: false,
+      icon: 'cowbell.png',
+      title: 'Cowbell'
+    })
   
     // and load the index.html of the app.
     win.loadURL(url.format({
